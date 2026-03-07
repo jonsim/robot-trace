32be92d257ab1c7a359d36e6382aa9c5f04288d4
Jonathan Simmonds <jon@jonsim.com>
2026-03-07 17:03:01 +0000

    Bump version to 1.2.0

--- pabot_trace/__init__.py
+++ pabot_trace/__init__.py
@@ -1,4 +1,4 @@
 from .PabotTraceReporter import PabotTraceReporter as pabot_trace
 
 __all__ = ["pabot_trace"]
-__version__ = "1.1.1"
+__version__ = "1.2.0"
--- rebot_trace/__init__.py
+++ rebot_trace/__init__.py
@@ -1,4 +1,4 @@
 from .RebotTrace import RebotTrace as rebot_trace
 
 __all__ = ["rebot_trace"]
-__version__ = "1.1.1"
+__version__ = "1.2.0"
